0a868d5ba75b54ee239698b184eadafb4ba915ca
Eric Tung <libpronet@gmail.com>
2019-07-04 15:42:42 +0800

    Add support for vs2013

---
+++ pub/lib-d/_get-win-vs2013.bat
@@ -0,0 +1,14 @@
+@echo off
+set THIS_DIR=%~sdp0
+
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug\pro_msg_jni.dll    %THIS_DIR%windows-vs2013\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug\pro_msg_jni.lib    %THIS_DIR%windows-vs2013\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug\pro_msg_jni.map    %THIS_DIR%windows-vs2013\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug\pro_msg_jni.pdb    %THIS_DIR%windows-vs2013\x86\
+
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug64x\pro_msg_jni.dll %THIS_DIR%windows-vs2013\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug64x\pro_msg_jni.lib %THIS_DIR%windows-vs2013\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug64x\pro_msg_jni.map %THIS_DIR%windows-vs2013\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2013\_debug64x\pro_msg_jni.pdb %THIS_DIR%windows-vs2013\x86_64\
+
+pause
